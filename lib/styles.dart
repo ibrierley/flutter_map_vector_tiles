e5718699d8e0d2dbc5302bb634ef7e92ed3566ab
Spencer Mein <spencer.mein@gmail.com>
2021-05-03 09:02:27 -0500

    code cleanup
    Remove some unused variables.
    Remove retry package (is included in http ^0.13.2)
    Modify gitignore
    change text font size to default (14)

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
 import 'dart:convert';
 
 import 'package:flutter/services.dart' show rootBundle;
